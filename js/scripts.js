089532b005b9d721cfe7dfe3943885e9a8064ed1
Nejik <nejikrofl@gmail.com>
2014-06-29 19:47:38 +0300

    v1.1
    remove unnecessary files

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-window.$ = function (selector, context) {
-	var el = context?context:document['querySelectorAll'](selector);
+//yes, i know that native methods like getElement... faster, but this is minimalistic library, if you want, you can improve it yourself
+
+window.j = function (selector, context) {
+	var el = ((context)?context:document)['querySelectorAll'](selector);
+
 	return (el.length < 2) ? el[0]: el;
 }
+
+if(!window.$) window.$ = window.j;
 
 var el = window.Element.prototype,
 	nl = window.NodeList.prototype;
@@ -10,6 +15,7 @@ var el = window.Element.prototype,
 nl.each = Array.prototype.forEach;
 
 // probably the most useful and allows $('#iddiv').find('.inside')
+// el.find = function(selector) { return $(selector, this) };
 el.find = function(selector) { return $(selector, this) };
 
 el.closest = function (selector) {
@@ -27,10 +33,6 @@ el.closest = function (selector) {
 	}
 	return undefined;
 }
-
-//if need has/add/remove class for ie9, add polyfill | /*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js*/
-// ;if("document" in self&&!("classList" in document.createElement("_"))){(function(j){"use strict";if(!("Element" in j)){return}var a="classList",f="prototype",m=j.Element[f],b=Object,k=String[f].trim||function(){return this.replace(/^\s+|\s+$/g,"")},c=Array[f].indexOf||function(q){var p=0,o=this.length;for(;p<o;p++){if(p in this&&this[p]===q){return p}}return -1},n=function(o,p){this.name=o;this.code=DOMException[o];this.message=p},g=function(p,o){if(o===""){throw new n("SYNTAX_ERR","An invalid or illegal string was specified")}if(/\s/.test(o)){throw new n("INVALID_CHARACTER_ERR","String contains an invalid character")}return c.call(p,o)},d=function(s){var r=k.call(s.getAttribute("class")||""),q=r?r.split(/\s+/):[],p=0,o=q.length;for(;p<o;p++){this.push(q[p])}this._updateClassName=function(){s.setAttribute("class",this.toString())}},e=d[f]=[],i=function(){return new d(this)};n[f]=Error[f];e.item=function(o){return this[o]||null};e.contains=function(o){o+="";return g(this,o)!==-1};e.add=function(){var s=arguments,r=0,p=s.length,q,o=false;do{q=s[r]+"";if(g(this,q)===-1){this.push(q);o=true}}while(++r<p);if(o){this._updateClassName()}};e.remove=function(){var t=arguments,s=0,p=t.length,r,o=false;do{r=t[s]+"";var q=g(this,r);if(q!==-1){this.splice(q,1);o=true}}while(++s<p);if(o){this._updateClassName()}};e.toggle=function(p,q){p+="";var o=this.contains(p),r=o?q!==true&&"remove":q!==false&&"add";if(r){this[r](p)}return !o};e.toString=function(){return this.join(" ")};if(b.defineProperty){var l={get:i,enumerable:true,configurable:true};try{b.defineProperty(m,a,l)}catch(h){if(h.number===-2146823252){l.enumerable=false;b.defineProperty(m,a,l)}}}else{if(b[f].__defineGetter__){m.__defineGetter__(a,i)}}}(self))};
-
 
 // $().addClass('name1 name2 name3');
 el.addClass = function(classes) {
@@ -52,6 +54,8 @@ nl.addClass = function(classes){
 	});
 	return this;
 };
+
+
 
 // $().removeClass('name1 name2 name3');
 el.removeClass = function(classes) {
@@ -75,6 +79,8 @@ nl.removeClass = function(classes){
 	return this;
 };
 
+
+
 // $().hasClass('name');
 el.hasClass = function(name) {
 	return this.classList.contains(name);
@@ -88,10 +94,156 @@ nl.hasClass = function(name) {
 	return success;
 };
 
+
+
+// $().attr('prop', 'value') support
+el.attr = function(name, value) {
+	if(value) {
+		this.setAttribute(name, value);
+		return this;
+	} else {
+		return this.getAttribute(name);
+	}
+};
+
+nl.attr = function(name, value) {
+	this.each(function(el) {
+		if(value) {
+			el.setAttribute(name, value);
+		} else {
+			return el.getAttribute(name);
+		}
+	});
+	return this;
+};
+
+
+
+// $().css('prop', 'value') support
+el.css = function(prop, value) {
+	if (value) {
+		this.style[prop] = value;
+		return this;
+	} else {
+		// return getStyle(this,prop);
+		return getComputedStyle(this, null)[prop];;
+	}
+};
+
+//.css() on nodelists works only for set, get not work
+nl.css = function(prop, value) {
+	this.each(function(el) {
+		el.css(prop, value);
+	});
+	return this;
+};
+
+
+
+//.html() works as setter, if you want get html, use el.innerHTML
+el.html = function (html) {
+	this.innerHTML = html;
+	return this;
+}
+nl.html = function(html) {
+	this.each(function(el) {
+		el.innerHTML = html;
+	});
+	return this;
+};
+
+
+
+el.remove = function () {
+	this.parentNode.removeChild(this)
+	return this;
+}
+
+nl.remove = function() {
+	this.each(function(el) {
+		el.parentNode.removeChild(el)
+	});
+	return this;
+};
+
+
+
+el.append = window.Element.prototype.appendChild;
+
+
+
+el.prepend = function (el) {
+	return this.insertBefore(el, this.firstChild);
+}
+
+
+el.before = function (el) {
+	return this.parentNode.insertBefore(el, this);
+}
+
+
+el.after = function (el) {
+	return this.parentNode.insertBefore(el, this.nextSibling);
+}
+
+
+//.on()
+el.on = function ( type, callback) {
+	if (this.addEventListener) {
+		this.addEventListener(type, callback, false);
+	} else if(this.attachEvent) {
+		this.attachEvent('on' + type, callback);
+	} else {
+		if( this[type] ){
+			this[type] = callback;
+		}
+	}
+	return this;
+}
+
+// el.is = function ( selector ){
+// 	var elements = document.querySelectorAll( selector );
+// 	for( index in elements ){
+// 		if( elements[index] === this ){
+// 			return true;
+// 		}
+// 	}
+// 	return false;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///if ie < 10
+//if need has/add/remove class for ie8-9, add polyfill | /*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js*/
+// ;if("document" in self&&!("classList" in document.createElement("_"))){(function(j){"use strict";if(!("Element" in j)){return}var a="classList",f="prototype",m=j.Element[f],b=Object,k=String[f].trim||function(){return this.replace(/^\s+|\s+$/g,"")},c=Array[f].indexOf||function(q){var p=0,o=this.length;for(;p<o;p++){if(p in this&&this[p]===q){return p}}return -1},n=function(o,p){this.name=o;this.code=DOMException[o];this.message=p},g=function(p,o){if(o===""){throw new n("SYNTAX_ERR","An invalid or illegal string was specified")}if(/\s/.test(o)){throw new n("INVALID_CHARACTER_ERR","String contains an invalid character")}return c.call(p,o)},d=function(s){var r=k.call(s.getAttribute("class")||""),q=r?r.split(/\s+/):[],p=0,o=q.length;for(;p<o;p++){this.push(q[p])}this._updateClassName=function(){s.setAttribute("class",this.toString())}},e=d[f]=[],i=function(){return new d(this)};n[f]=Error[f];e.item=function(o){return this[o]||null};e.contains=function(o){o+="";return g(this,o)!==-1};e.add=function(){var s=arguments,r=0,p=s.length,q,o=false;do{q=s[r]+"";if(g(this,q)===-1){this.push(q);o=true}}while(++r<p);if(o){this._updateClassName()}};e.remove=function(){var t=arguments,s=0,p=t.length,r,o=false;do{r=t[s]+"";var q=g(this,r);if(q!==-1){this.splice(q,1);o=true}}while(++s<p);if(o){this._updateClassName()}};e.toggle=function(p,q){p+="";var o=this.contains(p),r=o?q!==true&&"remove":q!==false&&"add";if(r){this[r](p)}return !o};e.toString=function(){return this.join(" ")};if(b.defineProperty){var l={get:i,enumerable:true,configurable:true};try{b.defineProperty(m,a,l)}catch(h){if(h.number===-2146823252){l.enumerable=false;b.defineProperty(m,a,l)}}}else{if(b[f].__defineGetter__){m.__defineGetter__(a,i)}}}(self))};
+
+//or
+
 //old ie classes
 //.hasClass()
-
-
 // window.Element.prototype.hasClass = function ( name ){
 // 	return this.className.split( ' ' ).indexOf( name ) > -1
 // }
@@ -124,40 +276,15 @@ nl.hasClass = function(name) {
 //   }
 // }
 
-// $().attr('prop', 'value') support
-el.attr = function(name, value) {
-	if(value) {
-		this.setAttribute(name, value);
-		return this;
-	} else {
-		return this.getAttribute(name);
-	}
-};
 
-nl.attr = function(name, value) {
-	this.each(function(el) {
-		if(value) {
-			el.setAttribute(name, value);
-		} else {
-			return el.getAttribute(name);
-		}
-	});
-	return this;
-};
 
-// $().css('prop', 'value') support
 
-el.css = function(prop, value) {
-	if (value) {
-		this.style[prop] = value;
-		return this;
-	} else {
-		// return getStyle(this,prop);
-		return getComputedStyle(this, null)[prop];;
-	}
-};
 
-//version for ie < 9
+
+
+
+
+//if you need .css() in ie < 9, uncomment this
 // el.css = function(prop, value) {
 // 	function getStyle(el, prop) {
 // 		var computedStyle;
@@ -181,53 +308,12 @@ el.css = function(prop, value) {
 // 	}
 // };
 
-//.css() on nodelists works only for set, get not work
-nl.css = function(prop, value) {
-	this.each(function(el) {
-		el.css(prop, value);
-	});
-	return this;
-};
-
-//.html() works as setter, if you want get html, use el.innerHTML
-el.html = function (html) {
-	this.innerHTML = html;
-	return this;
-}
-nl.html = function(html) {
-	this.each(function(el) {
-		el.innerHTML = html;
-	});
-	return this;
-};
-
-el.remove = function () {
-	this.parentNode.removeChild(this)
-	return this;
-}
-
-nl.remove = function() {
-	this.each(function(el) {
-		el.parentNode.removeChild(el)
-	});
-	return this;
-};
-
-el.append = window.Element.prototype.appendChild;
-
-el.prepend = function (el) {
-	return this.insertBefore(el, this.firstChild);
-}
-el.before = function (el) {
-	return this.parentNode.insertBefore(el, this);
-}
-el.after = function (el) {
-	return this.parentNode.insertBefore(el, this.nextSibling);
-}
 
 
-var t = $('.testClass').css('color');
 
-console.log(t)
-// t.remove();
+
+
+
+
+
 
